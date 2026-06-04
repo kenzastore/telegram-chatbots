822b47008ecf18d01f1818768e2bb702f9564605
kenzastore <kenzastoreid@gmail.com>
2026-06-04 17:24:34 +0700

    feat(bot): Add /quick to persistent shortcut keyboard menu

--- bot.py
+++ bot.py
@@ -56,6 +56,7 @@ def get_commands_keyboard() -> ReplyKeyboardMarkup:
     """Returns a ReplyKeyboardMarkup featuring shortcuts for all main bot commands."""
     keyboard = [
         ["/add", "/balance"],
+        ["/quick"],
         ["/view", "/summary"],
         ["/edit", "/clear"],
         ["/help"]
